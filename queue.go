7469f179747980853000f57e9426399aa9e49bd5
dumberic <502816582@qq.com>
2017-02-19 15:26:05 +0800

    [MODIFY] modify

--- queue.go
+++ queue.go
@@ -1,7 +1,6 @@
-package queue
+package Queue
 
 import (
-	"fmt"
 	"time"
 )
 
@@ -21,25 +20,26 @@ func (this *Queue) Push(s Message) {
 	this.size = this.size + 1
 }
 
-func (this *Queue) PushNext(s []Message, index int) {
+func (this *Queue) PushNext(msg Message, index int) {
+	var arry []Message = make([]Message, 0)
+	arry = append(arry, msg)
 	if this.size == 0 {
-		this.queue = s
+		this.queue = arry
 		this.size = this.size + 1
 		return
 	}
 	before := this.queue[:index+1]
 	after := this.queue[index+1:]
-	fmt.Println(this.size, after)
-	this.queue = append(before, append(s, after...)...)
+	this.queue = append(before, append(arry, after...)...)
 	this.size = this.size + 1
 }
 
 //队列先进先出
 func (this *Queue) Pop() Message {
 	s := this.queue[0:1]
-	this.queue = this.queue[1:this.size]
+	this.queue = this.queue[1:]
 	this.size = this.size - 1
-	//	logger.Debug(s[0].Message)
+	// log.Println(s[0])
 	return s[0]
 }
 
@@ -51,7 +51,7 @@ func (this *Queue) Pull() Message {
 	return s[0]
 }
 
-func (this *Queue) GetSize() int {
+func (this *Queue) Size() int {
 	return this.size
 }
 
@@ -63,30 +63,24 @@ func (this *Queue) List() []Message {
 	return this.queue
 }
 
-func (this *Queue) FindById(id string) []int {
-	var find []int
+func (this *Queue) FindById(id string) (index int, msg Message) {
 	if this.size <= 0 {
-		return find
+		return -1, msg
 	}
-	for i, _ := range this.queue {
-		if msg := this.queue[i : i+1][0]; id == msg.Id {
-			find = append(find, i)
+	for k, v := range this.queue {
+		if v.Id == id {
+			return k, this.queue[k : k+1][0]
 		}
 	}
-	return find
+	return -1, msg
 }
 
-func (this *Queue) DelById(id string) bool {
+func (this *Queue) DeleteById(id string) int {
 	if this.size <= 0 {
-		return false
+		return -1
 	}
-	var findIndex []int = this.FindById(id)
-	if len(findIndex) <= 0 {
-		return false
-	}
-
-	var firstFind int = findIndex[0]
-	this.queue = append(this.queue[:firstFind], this.queue[firstFind+1:]...)
+	var index, _ = this.FindById(id)
+	this.queue = append(this.queue[:index], this.queue[index+1:]...)
 	this.size = this.size - 1
-	return true
+	return index
 }
